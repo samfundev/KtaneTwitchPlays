569c12d6286f32f48ea9fc2d5bad192d7eb4419e
caitsith2 <d_good@caitsith2.com>
2017-12-06 03:31:57 -0800

    Use updated Vanilla Rule Modifier API
    Now displays the help appropriate to whether Vanilla Rule Modifier rules
    are in effect.

--- Assets/Scripts/ComponentSolvers/Vanilla/ButtonComponentSolver.cs
+++ Assets/Scripts/ComponentSolvers/Vanilla/ButtonComponentSolver.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using Assets.Scripts.Helpers;
 using UnityEngine;
 
 public class ButtonComponentSolver : ComponentSolver
