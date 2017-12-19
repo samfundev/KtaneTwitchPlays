0f711b3a4db40b6d609b0cee90f63e6856ce606a
samfun123 <samdinner@hotmail.com>
2017-12-18 19:30:58 -0500

    Read colors using values between 0-255 instead of 0-1.

--- Assets/Scripts/Helpers/SettingsConverter.cs
+++ Assets/Scripts/Helpers/SettingsConverter.cs
@@ -18,31 +18,35 @@ class SettingsConverter
 
 class ColorConverter : JsonConverter
 {
+	private int? ParseInt(string number)
+	{
+		int i;
+		return int.TryParse(number, out i) ? (int?) i : null;
+	}
+
 	public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
 	{
 		Color color = (Color) value;
-		if (color.a == 1) writer.WriteValue(string.Format("{0}, {1}, {2}", color.r, color.g, color.b));
-		else writer.WriteValue(string.Format("{0}, {1}, {2}, {3}", color.r, color.g, color.b, color.a));
+		string format = string.Format("{0}, {1}, {2}", (int) (color.r * 255), (int) (color.g * 255), (int) (color.b * 255));
+		if (color.a != 1) format += ", " + (int) (color.a * 255);
+
+		writer.WriteValue(format);
 	}
 
 	public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 	{
-		try
+		var parts = ((string) reader.Value).Split(',').Select(str => ParseInt(str.Trim()));
+		if (parts.Any(x => x == null)) return existingValue;
+
+		var values = parts.Select(i => (int) i / 255f).ToArray();
+		switch (values.Count())
 		{
-			var values = ((string) reader.Value).Split(',').Select(x => float.Parse(x.Trim())).ToArray();
-			switch (values.Count())
-			{
-				case 3:
-					return new Color(values[0], values[1], values[2]);
-				case 4:
-					return new Color(values[0], values[1], values[2], values[3]);
-				default:
-					return null;
-			}
-		}
-		catch
-		{
-			return null;
+			case 3:
+				return new Color(values[0], values[1], values[2]);
+			case 4:
+				return new Color(values[0], values[1], values[2], values[3]);
+			default:
+				return existingValue;
 		}
 	}
 
