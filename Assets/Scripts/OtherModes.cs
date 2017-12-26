9574670dd6afc49f53ad9139b4d228c5a77c4f58
elias5891 <elias5891@gmail.com>
2017-12-26 11:41:24 -0500

    Add bombtime mode stuff.

---
+++ Assets/Scripts/OtherModes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class OtherModes
+{
+    public static bool timedModeOn = false;
+    public static float timedMultiplier = 9;
+
+    public static void toggleTimedMode()
+    {
+        timedModeOn = !timedModeOn;
+    }
+
+    public static bool timedModeCheck()
+    {
+        return timedModeOn;
+    }
+
+    public static float getMultiplier()
+    {
+        return timedMultiplier;
+    }
+
+    public static bool dropMultiplier()
+    {
+        if (timedMultiplier > 2.5)
+        {
+            timedMultiplier = timedMultiplier - 1.5f;
+            return true;
+        }
+        else
+        {
+            timedMultiplier = 1;
+            return false;
+        }
+    }
+
+    public static void setMultiplier(float newMultiplier)
+    {
+        timedMultiplier = newMultiplier;
+    }
+}
