cfe05889acd5bf79c2959c9e567eab277e2e0bf8
caitsith2 <d_good@caitsith2.com>
2018-01-05 23:05:37 -0800

    Add option for Turning the key at the incorrect time
    at the cost of a strike, and disable continuous strikes every two
    seconds for missing the Key.

--- Assets/Scripts/ComponentSolvers/Modded/Perky/TurnTheKeyComponentSolver.cs
+++ Assets/Scripts/ComponentSolvers/Modded/Perky/TurnTheKeyComponentSolver.cs
@@ -9,8 +9,44 @@ public class TurnTheKeyComponentSolver : ComponentSolver
     public TurnTheKeyComponentSolver(BombCommander bombCommander, MonoBehaviour bombComponent, IRCConnection ircConnection, CoroutineCanceller canceller) :
         base(bombCommander, bombComponent, ircConnection, canceller)
     {
-        _lock = (MonoBehaviour)_lockField.GetValue(bombComponent.GetComponent(_componentType));
+        _lock = (MonoBehaviour)_lockField.GetValue(BombComponent.GetComponent(_componentType));
         modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType());
+        BombCommander.twitchBombHandle.StartCoroutine(ReWriteTurnTheKey());
+    }
+
+    private bool OnKeyTurn()
+    {
+        _onKeyTurnMethod.Invoke(BombComponent.GetComponent(_componentType), null);
+        if (TwitchPlaySettings.data.AllowTurnTheKeyEarlyLate && !(bool)_solvedField.GetValue(BombComponent.GetComponent(_componentType)))
+        {
+            int time = (int)_targetTimeField.GetValue(BombComponent.GetComponent(_componentType));
+            float currentBombTime = BombCommander.CurrentTimer;
+            CommonReflectedTypeInfo.TimeRemainingField.SetValue(BombCommander.timerComponent, time + 0.5f);
+            _onKeyTurnMethod.Invoke(BombComponent.GetComponent(_componentType), null);
+            CommonReflectedTypeInfo.TimeRemainingField.SetValue(BombCommander.timerComponent, currentBombTime);
+        }
+        return false;
+    }
+
+    private IEnumerator ReWriteTurnTheKey()
+    {
+        yield return new WaitUntil(() => (bool) _activatedField.GetValue(BombComponent.GetComponent(_componentType)));
+        yield return new WaitForSeconds(0.1f);
+        _stopAllCorotinesMethod.Invoke(BombComponent.GetComponent(_componentType), null);
+
+        ((KMSelectable)_lock).OnInteract = OnKeyTurn;
+        int expectedTime = (int)_targetTimeField.GetValue(BombComponent.GetComponent(_componentType));
+        while (true)
+        {
+            int time = Mathf.FloorToInt(BombCommander.CurrentTimer);
+            if (time < expectedTime &&
+                !(bool)_solvedField.GetValue(BombComponent.GetComponent(_componentType)) &&
+                !TwitchPlaySettings.data.AllowTurnTheKeyEarlyLate)
+            {
+                BombComponent.GetComponent<KMBombModule>().HandleStrike();
+            }
+            yield return new WaitForSeconds(2.0f);
+        }
     }
 
     protected override IEnumerator RespondToCommandInternal(string inputCommand)
@@ -93,10 +129,20 @@ public class TurnTheKeyComponentSolver : ComponentSolver
     {
         _componentType = ReflectionHelper.FindType("TurnKeyModule");
         _lockField = _componentType.GetField("Lock", BindingFlags.Public | BindingFlags.Instance);
+        _activatedField = _componentType.GetField("bActivated", BindingFlags.NonPublic | BindingFlags.Instance);
+        _solvedField = _componentType.GetField("bUnlocked", BindingFlags.NonPublic | BindingFlags.Instance);
+        _targetTimeField = _componentType.GetField("mTargetSecond", BindingFlags.NonPublic | BindingFlags.Instance);
+        _stopAllCorotinesMethod = _componentType.GetMethod("StopAllCoroutines", BindingFlags.Public | BindingFlags.Instance);
+        _onKeyTurnMethod = _componentType.GetMethod("OnKeyTurn", BindingFlags.NonPublic | BindingFlags.Instance);
     }
 
     private static Type _componentType = null;
     private static FieldInfo _lockField = null;
+    private static FieldInfo _activatedField = null;
+    private static FieldInfo _solvedField = null;
+    private static FieldInfo _targetTimeField = null;
+    private static MethodInfo _stopAllCorotinesMethod = null;
+    private static MethodInfo _onKeyTurnMethod = null;
 
     private MonoBehaviour _lock = null;
 }
--- Assets/Scripts/TwitchPlaySettings.cs
+++ Assets/Scripts/TwitchPlaySettings.cs
@@ -29,6 +29,7 @@ public class TwitchPlaySettingsData
 	public bool EnableTwitchPlayShims = true;
 	public float UnsubmittablePenaltyPercent = 0.3f;
 	public Color UnclaimedColor = new Color(0.39f, 0.25f, 0.64f);
+    public bool AllowTurnTheKeyEarlyLate = true;
 
 	public Dictionary<string, string> CustomMissions = new Dictionary<string, string>();
 	public List<string> ProfileWhitelist = new List<string>();
