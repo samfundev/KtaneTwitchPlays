03a40d0302b701aeb2682ea2c32861be5124afc6
caitsith2 <d_good@caitsith2.com>
2018-01-17 13:16:37 -0800

    Fix possible exceptions in Reflection helper.

--- TwitchPlaysAssembly/Src/Helpers/ReflectionHelper.cs
+++ TwitchPlaysAssembly/Src/Helpers/ReflectionHelper.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 public static class ReflectionHelper
 {
     public static Type FindType(string fullName)
     {
-        return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName.Equals(fullName));
+        return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetSafeTypes()).FirstOrDefault(t => t.FullName != null && t.FullName.Equals(fullName));
     }
 
     public static Type FindType(string fullName, string assemblyName)
     {
-        return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName.Equals(fullName) && t.Assembly.GetName().Name.Equals(assemblyName));
+        return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetSafeTypes()).FirstOrDefault(t => t.FullName != null && t.FullName.Equals(fullName) && t.Assembly.GetName().Name.Equals(assemblyName));
     }
+
+	private static IEnumerable<Type> GetSafeTypes(this Assembly assembly)
+	{
+		try
+		{
+			return assembly.GetTypes();
+		}
+		catch (ReflectionTypeLoadException e)
+		{
+			return e.Types.Where(x => x != null);
+		}
+		catch (Exception)
+		{
+			return new List<Type>();
+		}
+	}
 }
