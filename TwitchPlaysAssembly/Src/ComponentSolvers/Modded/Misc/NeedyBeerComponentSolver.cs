967b1223a770e5f14101a7298d54180dfc02bbea
caitsith2 <d_good@caitsith2.com>
2018-02-04 11:19:23 -0800

    Fixed logic bugs in Needy Beer suppot

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/NeedyBeerComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/NeedyBeerComponentSolver.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 
 public class NeedyBeerComponentSolver : ComponentSolver
 {
 	public NeedyBeerComponentSolver(BombCommander bombCommander, BombComponent bombComponent, IRCConnection ircConnection, CoroutineCanceller canceller) 
 		: base(bombCommander, bombComponent, ircConnection, canceller)
 	{
+		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType());
 	}
 
 	protected override IEnumerator RespondToCommandInternal(string inputCommand)
@@ -13,7 +15,7 @@ public class NeedyBeerComponentSolver : ComponentSolver
 		if (inputCommand.ToLowerInvariant().EqualsAny("refill"))
 		{
 			yield return null;
-			yield return DoInteractionClick(BombComponent.GetComponentInChildren<KMSelectable>());
+			yield return DoInteractionClick(BombComponent.GetComponentsInChildren<KMSelectable>().FirstOrDefault(x => x.name.Equals("Solve")));
 		}
 	}
 }
