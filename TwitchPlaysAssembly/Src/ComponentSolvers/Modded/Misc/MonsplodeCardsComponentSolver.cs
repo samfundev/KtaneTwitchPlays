b768da16794e6700243b44fb1359686d9e88152c
rocket0634 <4911928+rocket0634@users.noreply.github.com>
2018-03-02 00:14:00 -0800

    Clarify work
    Add !clarify to Monsplode Trading Cards
    Begin work on new clarify command for Web Design.

---
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/MonsplodeCardsComponentSolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+public class MonsplodeCardsComponentSolver : ComponentSolver
+{
+	public MonsplodeCardsComponentSolver(BombCommander bombCommander, BombComponent bombComponent) :
+	base(bombCommander, bombComponent)
+	{
+		_component = bombComponent.GetComponent(_componentType);
+		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType());
+		Names[0] = (TextMesh)_Names[0].GetValue(_component);
+		Names[1] = (TextMesh)_Names[1].GetValue(_component);
+	}
+
+	protected override IEnumerator RespondToCommandInternal(string inputCommand)
+	{
+		if (inputCommand.ToLowerInvariant().Equals("clarify left"))
+		{
+			yield return null;
+			yield return $"sendtochat {string.Format("Owned: {0}", Names[0].text)}";
+		}
+		else if (inputCommand.ToLowerInvariant().Equals("clarify right"))
+		{
+			yield return null;
+			yield return $"sendtochat {string.Format("Offered: {0}", Names[1].text)}";
+		}
+		else if (inputCommand.ToLowerInvariant().Equals("clarify"))
+		{
+			yield return null;
+			yield return $"sendtochat {string.Format("Currently viewing owned: {0} offered: {1}", Names[0].text, Names[1].text)}";
+		}
+		else
+		{
+			yield return null;
+			IEnumerator command = (IEnumerator)_ProcessCommandMethod.Invoke(_component, new object[] { inputCommand });
+			yield return command;
+		}
+	}
+
+	static MonsplodeCardsComponentSolver()
+	{
+		_componentType = ReflectionHelper.FindType("MonsplodeCardModule");
+		_ProcessCommandMethod = _componentType.GetMethod("ProcessTwitchCommand", BindingFlags.NonPublic | BindingFlags.Instance);
+		_Names[0] = _componentType.GetField("deckTM", BindingFlags.Public | BindingFlags.Instance);
+		_Names[1] = _componentType.GetField("offerTM", BindingFlags.Public | BindingFlags.Instance);
+	}
+
+	private static Type _componentType = null;
+	private static MethodInfo _ProcessCommandMethod = null;
+	private TextMesh[] Names = new TextMesh[2];
+
+	private object _component = null;
+	private static FieldInfo[] _Names = new FieldInfo[2];
+}
---
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/WebDesignComponentSolver.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+public class WebDesignComponentSolver : ComponentSolver
+{
+	public WebDesignComponentSolver(BombCommander bombCommander, BombComponent bombComponent) :
+	base(bombCommander, bombComponent)
+	{
+		_component = bombComponent.GetComponent(_componentType);
+		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType());
+		text = (TextMesh)_text.GetValue(_component);
+		btns = (KMSelectable[])_btn.GetValue(_component);
+	}
+
+	private void FitToWidth()
+	{
+		screenHolder = text.text;
+		foreach (char c in screenHolder)
+		{
+			if (c == '\n') screenHolder.Replace("\n", "");
+		}
+		string[] words = screenHolder.Split(" "[0]);
+		newScreen = "";
+		string line = "";
+		var count = 0;
+
+		foreach(string s in words)
+		{
+			string temp = line + " " + s;
+			if (count > wantedHeight) { screenHolder = temp; text.text = newScreen.Substring(1, newScreen.Length - 1); return; }
+			else if (temp.Length > wantedWidth)
+			{
+				newScreen += line + "\n";
+				line = s;
+				count++;
+			}
+			else { line = temp; }
+		}
+		newScreen += line;
+		text.text = newScreen.Substring(1, newScreen.Length - 1);
+		screenHolder = "";
+	}
+
+	protected override IEnumerator RespondToCommandInternal(string inputCommand)
+	{
+		if (inputCommand.Equals("clarify", StringComparison.InvariantCultureIgnoreCase)) 
+		{
+			if (!active)
+			{
+				yield return null;
+				var p = text.transform.localPosition;
+				oldScreen = text.text;
+				active = true;
+				text.fontSize = 30;
+				text.transform.localPosition = new Vector3(p.x + 0.024f, p.y, p.z);
+				FitToWidth();
+				if (!(screenHolder == ""))
+				{
+					while (!(screenHolder == ""))
+					{
+						yield return new WaitForSeconds(10f);
+						text.text = screenHolder;
+						FitToWidth();
+					}
+				}
+				yield return null;
+
+			}
+			else if (active)
+			{
+				yield return null;
+				var p = text.transform.localPosition;
+				active = false;
+				text.fontSize = 15;
+				text.text = oldScreen;
+				text.transform.localPosition = new Vector3(p.x - 0.024f, p.y, p.z);
+				yield return null;
+			}
+		}
+
+		else
+		{
+			/* // For some reason this part isn't working, so reimplementation for now.
+			  KMSelectable[] command = (KMSelectable[])_ProcessCommandMethod.Invoke(_component, new object[] { inputCommand });
+			if (command == null) yield break;
+			yield return null;
+			yield return command;*/
+			switch (inputCommand.ToLowerInvariant().Trim())
+			{
+				case "accept":
+				case "acc":
+					yield return null;
+					yield return DoInteractionClick(btns[0]);
+					yield break;
+				case "consider":
+				case "con":
+					yield return null;
+					yield return DoInteractionClick(btns[1]);
+					yield break;
+				case "reject":
+				case "rej":
+					yield return null;
+					yield return DoInteractionClick(btns[2]);
+					yield break;
+				default:
+					yield return null;
+					yield break;
+			}
+		}
+	}
+	static WebDesignComponentSolver()
+	{
+		_componentType = ReflectionHelper.FindType("webdesign");
+		//_ProcessCommandMethod = _componentType.GetMethod("ProcessTwitchCommand", BindingFlags.NonPublic | BindingFlags.Instance );
+		_text = _componentType.GetField("text", BindingFlags.Public | BindingFlags.Instance);
+		_btn = _componentType.GetField("btn", BindingFlags.Public | BindingFlags.Instance);
+	}
+
+	private static Type _componentType = null;
+	//private static MethodInfo _ProcessCommandMethod = null;
+	private static FieldInfo _btn = null;
+	private KMSelectable[] btns = null;
+	private static FieldInfo _text = null;
+	private TextMesh text = null;
+	private bool active;
+	private string oldScreen = null;
+	private string newScreen = null;
+	private string screenHolder = null;
+
+	private object _component = null;
+	private int wantedWidth = 15;
+	private int wantedHeight = 6;
+}
