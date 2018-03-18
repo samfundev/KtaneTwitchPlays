73ae59c23adfff3ebdf179fd031fbe21828a4766
caitsith2 <d_good@caitsith2.com>
2018-03-18 13:39:27 -0700

    Turns out Hexi implemented Forget me not module side, but module side
    unfortunately has a bug that it rejects !# press 98738728... as invalid
    input, expecting !# submit 3883871... instead. Converted this to a
    dedicated shim for now.

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Hexi/ForgetMeNotComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Hexi/ForgetMeNotComponentSolver.cs
@@ -9,26 +9,43 @@ public class ForgetMeNotComponentSolver : ComponentSolver
 	public ForgetMeNotComponentSolver(BombCommander bombCommander, BombComponent bombComponent) :
 		base(bombCommander, bombComponent)
 	{
-		_buttons = (Array)_buttonsField.GetValue(bombComponent.GetComponent(_componentType));
-		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), "Enter forget me not sequence with !{0} press 5 3 1 8 2 0... The Sequence length depends on how many modules were on the bomb.");
+		_buttons = (Array) _buttonsField.GetValue(bombComponent.GetComponent(_componentType));
+		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), (string)_twitchPlaysHelpField.GetValue(BombComponent.GetComponent(_componentType)));
 	}
+
+	protected override bool HandleForcedSolve()
+	{
+		CoroutineQueue.AddForcedSolve(HandleForcedSolveIEnumerator());
+		return true;
+	}
+
+	public IEnumerator HandleForcedSolveIEnumerator()
+	{
+		TextMesh displayMesh = (TextMesh) _stageMeshField.GetValue(BombComponent.GetComponent(_componentType));
+		int[] solution = (int[]) _solutionField.GetValue(BombComponent.GetComponent(_componentType));
+		while (!displayMesh.text.Equals("--"))
+			yield return true;
+
+		while (Position < solution.Length)
+		{
+			MonoBehaviour button = (MonoBehaviour)_buttons.GetValue(solution[Position]);
+			yield return DoInteractionClick(button);
+		}
+	}
+
+	private int Position => (int) _positionField.GetValue(BombComponent.GetComponent(_componentType));
+
 
 	protected override IEnumerator RespondToCommandInternal(string inputCommand)
 	{
-		if (!inputCommand.RegexMatch(out Match match, "^press ([0-9 ]+)$"))
+		if (!inputCommand.RegexMatch(out Match match, "^(?:press|submit) ([0-9 ]+)$"))
 		{
 			yield break;
 		}
-
-		yield return null;
-		foreach (char buttonString in match.Groups[1].Value)
+		IEnumerator processTwitchCommand = (IEnumerator) _twitchPlaysMethod.Invoke(BombComponent.GetComponent(_componentType), new object[] {$"submit {match.Groups[1].Value}"});
+		while (processTwitchCommand.MoveNext())
 		{
-			int val = buttonString - '0';
-			if (val < 0 || val > 9) continue;
-			MonoBehaviour button = (MonoBehaviour)_buttons.GetValue(val);
-
-			yield return $"trycancel The entry of Forget me not sequence was cancelled.";
-			yield return DoInteractionClick(button);
+			yield return processTwitchCommand.Current;
 		}
 	}
 
@@ -36,10 +53,22 @@ public class ForgetMeNotComponentSolver : ComponentSolver
 	{
 		_componentType = ReflectionHelper.FindType("AdvancedMemory");
 		_buttonsField = _componentType.GetField("Buttons", BindingFlags.NonPublic | BindingFlags.Instance);
+		_solutionField = _componentType.GetField("Solution", BindingFlags.NonPublic | BindingFlags.Instance);
+		_stageMeshField = _componentType.GetField("StageMesh", BindingFlags.Public | BindingFlags.Instance);
+		_positionField = _componentType.GetField("Position", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		_twitchPlaysMethod = _componentType.GetMethod("ProcessTwitchCommand", BindingFlags.Public | BindingFlags.Instance);
+		_twitchPlaysHelpField = _componentType.GetField("TwitchHelpMessage", BindingFlags.NonPublic | BindingFlags.Instance);
 	}
 
 	private static Type _componentType = null;
+
 	private static FieldInfo _buttonsField = null;
+	private static FieldInfo _solutionField = null;
+	private static FieldInfo _stageMeshField = null;
+	private static FieldInfo _positionField = null;
+	private static MethodInfo _twitchPlaysMethod = null;
+	private static FieldInfo _twitchPlaysHelpField = null;
 
 	private Array _buttons = null;
 }
