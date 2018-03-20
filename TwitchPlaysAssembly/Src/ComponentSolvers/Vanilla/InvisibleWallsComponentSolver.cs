4ddf3dfffd1f35a6c15245e074d55e83497fbe26
caitsith2 <d_good@caitsith2.com>
2018-03-19 22:46:02 -0700

    Forced solve handler for Maze

--- TwitchPlaysAssembly/Src/ComponentSolvers/Vanilla/InvisibleWallsComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Vanilla/InvisibleWallsComponentSolver.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Assets.Scripts.Rules;
 
 public class InvisibleWallsComponentSolver : ComponentSolver
 {
@@ -45,5 +46,56 @@ public class InvisibleWallsComponentSolver : ComponentSolver
         {"u", 0}, {"l", 1}, {"r", 2}, {"d", 3}
     };
 
-    private List<KeypadButton> _buttons = null;
+	private int GetLocationFromCell(MazeCell cell)
+	{
+		return (cell.Y * 10) + cell.X;
+	}
+
+	private readonly Stack<int> _mazeStack = new Stack<int>();
+	private bool[] _explored;
+	private bool GenerateMazeSolution(int startXY)
+	{
+		if (startXY == 77)
+		{
+			_explored = new bool[60];
+			startXY = GetLocationFromCell(((InvisibleWallsComponent) BombComponent).CurrentCell);
+		}
+		var endXY = GetLocationFromCell(((InvisibleWallsComponent) BombComponent).GoalCell);
+
+
+		var x = startXY % 10;
+		var y = startXY / 10;
+
+		if ((x > 5) || (y > 5) || (endXY == 66)) return false;
+		//var directions = _mazes[maze, y, x];
+		var cell = ((InvisibleWallsComponent) BombComponent).Maze.GetCell(x, y);
+		var directions = new[] { cell.WallAbove, cell.WallBelow, cell.WallLeft, cell.WallRight };
+		if (startXY == endXY) return true;
+		_explored[startXY] = true;
+
+		var directionInt = new[] { -10, 10, -1, 1 };
+		var directionReturn = new[] { 0, 3, 1, 2 };
+
+		for (var i = 0; i < 4; i++)
+		{
+			if (directions[i]) continue;
+			if (_explored[startXY + directionInt[i]]) continue;
+			if (!GenerateMazeSolution(startXY + directionInt[i])) continue;
+			_mazeStack.Push(directionReturn[i]);
+			return true;
+		}
+		return false;
+	}
+
+	protected override IEnumerator ForcedSolveIEnumerator()
+	{
+		yield return null;
+		while (!BombComponent.IsActive) yield return true;
+		if (BombComponent.IsSolved) yield break;
+		if (!GenerateMazeSolution(77)) yield break;
+		while (_mazeStack.Count > 0)
+			yield return DoInteractionClick(_buttons[_mazeStack.Pop()]);
+	}
+
+	private List<KeypadButton> _buttons = null;
 }
