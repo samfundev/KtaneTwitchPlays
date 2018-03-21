bc1ec9e6f6e1d62d11b35df6152d5b844e71f5a9
caitsith2 <d_good@caitsith2.com>
2018-03-21 00:17:28 -0700

    Reduced shim down to hold music and "check"

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Hexi/PlumbingComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Hexi/PlumbingComponentSolver.cs
@@ -13,48 +13,16 @@ public class PlumbingComponentSolver : ComponentSolverShim
 
     protected override IEnumerator RespondToCommandInternal(string inputCommand)
     {
-	    string[] sequence = inputCommand.ToUpperInvariant().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
-	    List<string> pipes = new List<string>();
-	    bool elevator = false;
+	    inputCommand = inputCommand.ToLowerInvariant();
+	    var sequence = inputCommand.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
 
-		inputCommand = inputCommand.ToLowerInvariant();
-
-        if (inputCommand.EqualsAny("submit", "check"))
-        {
-	        inputCommand = "submit";
-        }
-		else if (inputCommand.Equals("spinme"))
-        {
-	        elevator = true;
-        }
-		else if (inputCommand.StartsWith("rotate "))
-        {
-			foreach (string buttonString in sequence)
-			{
-				var letters = "ABCDEF";
-				var numbers = "123456";
-				if (buttonString.Length != 2 || letters.IndexOf(buttonString[0]) < 0 ||
-				    numbers.IndexOf(buttonString[1]) < 0)
-				{
-					yield return $"sendtochaterror Bad pipe position: '{buttonString}'";
-					yield break;
-				}
-
-				var row = numbers.IndexOf(buttonString[1]);
-				var col = letters.IndexOf(buttonString[0]);
-				var button = letters[row].ToString() + numbers[col];
-
-				pipes.Add(button);
-				elevator |= pipes.FindAll(x => x.Equals(button)).Count >= 4;
-			}
-	        inputCommand = $"rotate {pipes.Join()}";
-        }
-
-	    if (elevator)
+	    if (sequence.Any(x => sequence.Count(y => y.Equals(x)) >= 4) || inputCommand.Equals("spinme"))
 	    {
 		    yield return null;
 		    yield return "elevator music";
 	    }
+
+		if (inputCommand.Equals("check")) inputCommand = "submit";
 
 	    IEnumerator process = base.RespondToCommandInternal(inputCommand);
 	    while (process.MoveNext()) yield return process.Current;
