29be68279f829af511ced4ccce51c9a413c29667
caitsith2 <d_good@caitsith2.com>
2018-03-20 17:12:06 -0700

    Use the new ComponentSolverShim derived class on existing shims

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Shims/ExtendedPasswordComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Shims/ExtendedPasswordComponentSolver.cs
@@ -2,14 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using TwitchPlaysAssembly.ComponentSolvers.Modded.Shims;
 
-public class ExtendedPasswordComponentSolver : ComponentSolver
+public class ExtendedPasswordComponentSolver : ComponentSolverShim
 {
 	public ExtendedPasswordComponentSolver(BombCommander bombCommander, BombComponent bombComponent) :
 		base(bombCommander, bombComponent)
 	{
-		_component = bombComponent.GetComponent(_componentType);
 		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), "!{0} cycle 6 [cycle through the letters in column 6] | !{0} lambda [try to submit a word]");
 	}
 
@@ -25,7 +24,7 @@ public class ExtendedPasswordComponentSolver : ComponentSolver
 				if (!int.TryParse(cycle, out int spinnerIndex) || !alreadyCycled.Add(spinnerIndex) || spinnerIndex < 1 || spinnerIndex > 6)
 					continue;
 
-				IEnumerator spinnerCoroutine = (IEnumerator)_ProcessCommandMethod.Invoke(_component, new object[] { $"cycle {cycle}" });
+				IEnumerator spinnerCoroutine = base.RespondToCommandInternal($"cycle {cycle}");
 				while (spinnerCoroutine.MoveNext())
 				{
 					yield return spinnerCoroutine.Current;
@@ -34,28 +33,20 @@ public class ExtendedPasswordComponentSolver : ComponentSolver
 			}
 			yield break;
 		}
-
-		IEnumerator command = (IEnumerator)_ProcessCommandMethod.Invoke(_component, new object[] { inputCommand });
-		while (command.MoveNext())
+		else if (inputCommand.Trim().Length == 6)
 		{
-			yield return command.Current;
-			yield return "trycancel";
-		}
-		if (inputCommand.Trim().Length == 6)
-		{
+			IEnumerator command = base.RespondToCommandInternal(inputCommand);
+			while (command.MoveNext())
+			{
+				yield return command.Current;
+				yield return "trycancel";
+			}
 			yield return null;
 			yield return "unsubmittablepenalty";
 		}
+		else
+		{
+			yield return "sendtochaterror valid commands are 'cycle [columns]' or a 6 letter password to try.";
+		}
 	}
-
-	static ExtendedPasswordComponentSolver()
-	{
-		_componentType = ReflectionHelper.FindType("ExtendedPassword", "ExtendedPassword");
-		_ProcessCommandMethod = _componentType.GetMethod("ProcessTwitchCommand", BindingFlags.NonPublic | BindingFlags.Instance);
-	}
-
-	private static Type _componentType = null;
-	private static MethodInfo _ProcessCommandMethod = null;
-
-	private object _component = null;
 }
--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Shims/IceCreamConfirm.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Shims/IceCreamConfirm.cs
@@ -1,21 +1,19 @@
 using System;
 using System.Collections;
-using System.Reflection;
 using Newtonsoft.Json;
+using TwitchPlaysAssembly.ComponentSolvers.Modded.Shims;
 
-public class IceCreamConfirm : ComponentSolver
+public class IceCreamConfirm : ComponentSolverShim
 {
 	public IceCreamConfirm(BombCommander bombCommander, BombComponent bombComponent) :
 	base(bombCommander, bombComponent)
 	{
-		_component = bombComponent.GetComponent(_componentType);
-		string help = (string)_HelpMessageField.GetValue(_component);
-
-		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), help + " Check the opening hours with !{0} hours.");
+		_settings = new Settings();
+		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), ShimData.HelpMessage + " Check the opening hours with !{0} hours.");
 		KMModSettings modSettings = bombComponent.GetComponent<KMModSettings>();
 		try
 		{
-			settings = JsonConvert.DeserializeObject<Settings>(modSettings.Settings);
+			_settings = JsonConvert.DeserializeObject<Settings>(modSettings.Settings);
 		}
 		catch (Exception ex)
 		{
@@ -24,42 +22,29 @@ public class IceCreamConfirm : ComponentSolver
 			TwitchPlaySettings.WriteDataToFile();
 		}
 	}
+
 	protected override IEnumerator RespondToCommandInternal(string inputCommand)
 	{
 		if (inputCommand.ToLowerInvariant().Equals("hours"))
 		{
 			yield return !TwitchPlaySettings.data.ShowHours
 				? "sendtochat Sorry, hours are currently unavailable. Enjoy your ice cream!"
-				: $"sendtochat {(settings.openingTimeEnabled ? "We are open every other hour today." : "We're open all day today!")}";
+				: $"sendtochat {(_settings.OpeningTimeEnabled ? "We are open every other hour today." : "We're open all day today!")}";
 		}
 		else
 		{
-			IEnumerator command = (IEnumerator)_ProcessCommandMethod.Invoke(_component, new object[] { inputCommand });
-			if (command == null) yield break;
+			IEnumerator command = base.RespondToCommandInternal(inputCommand);
 			while (command.MoveNext())
 			{
 				yield return command.Current;
 			}
 		}
 	}
-	static IceCreamConfirm()
-	{
-		_componentType = ReflectionHelper.FindType("IceCreamModule");
-		_ProcessCommandMethod = _componentType.GetMethod("ProcessTwitchCommand", BindingFlags.Public | BindingFlags.Instance);
-		_HelpMessageField = _componentType.GetField("TwitchHelpMessage", BindingFlags.Public | BindingFlags.Instance);
 
-	}
-	class Settings
+	private class Settings
 	{
-#pragma warning disable 649
-		public bool openingTimeEnabled;
-#pragma warning restore 649
+		public bool OpeningTimeEnabled = false;
 	}
 
-	private static Type _componentType = null;
-	private static MethodInfo _ProcessCommandMethod = null;
-	private static FieldInfo _HelpMessageField = null;
-
-	private object _component = null;
-	private Settings settings;
+	private readonly Settings _settings;
 }
