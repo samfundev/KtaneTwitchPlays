9af494dc1ccdfc7ebfbe55506084c4422939dc5a
caitsith2 <d_good@caitsith2.com>
2018-03-23 10:48:01 -0700

    Add more Tuple types

--- TwitchPlaysAssembly/Src/Helpers/Tuple.cs
+++ TwitchPlaysAssembly/Src/Helpers/Tuple.cs
@@ -8,3 +8,31 @@
 		Second = second;
 	}
 }
+
+public class Tuple<T1, T2, T3>
+{
+	public T1 First { get; private set; }
+	public T2 Second { get; private set; }
+	public T3 Third { get; private set; }
+	internal Tuple(T1 first, T2 second, T3 third)
+	{
+		First = first;
+		Second = second;
+		Third = third;
+	}
+}
+
+public class Tuple<T1, T2, T3, T4>
+{
+	public T1 First { get; private set; }
+	public T2 Second { get; private set; }
+	public T3 Third { get; private set; }
+	public T4 Fourth { get; private set; }
+	internal Tuple(T1 first, T2 second, T3 third, T4 fourth)
+	{
+		First = first;
+		Second = second;
+		Third = third;
+		Fourth = fourth;
+	}
+}
