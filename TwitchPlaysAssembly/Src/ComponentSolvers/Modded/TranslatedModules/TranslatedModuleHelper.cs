05804e6e6f3553cd61b528ea19a3f516887d9ab5
PeterRed4091 <35828682+PeterRed4091@users.noreply.github.com>
2018-03-30 13:16:39 +0800

    New manual codes of Translated Modules

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/TranslatedModules/TranslatedModuleHelper.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/TranslatedModules/TranslatedModuleHelper.cs
@@ -7,14 +7,16 @@ public static class TranslatedModuleHelper
 {
 	private static readonly Dictionary<string, string> ManualCodeAddons = new Dictionary<string, string>()
 	{
-		{"en", ""},
+		{"en", "" },
 		{"cu", null },
+		{"cs", "%20translated%20full%20(%C4%8De%C5%A1tina)" },
 		{"da", "%20translated%20full%20(Dansk)" },
 		{"de", "%20translated%20full%20(Deutsch)" },
 		{"et", "%20translated%20full%20(eesti)" },
 		{"es", "%20translated%20full%20(Espa%C3%B1ol)" },
 		{"eo", "%20translated%20full%20(Esperanto)" },
 		{"fr", "%20translated%20full%20(Fran%C3%A7ais)" },
+		{"he", "%20translated%20full%20(%D7%A2%D7%91%D7%A8%D7%99%D7%AA)" },
 		{"ko", "%20translated%20full%20(%ED%95%9C%EA%B5%AD%EC%96%B4)" },
 		{"it", "%20translated%20full%20(Italiano)" },
 		{"nl", "%20translated%20full%20(Nederlands)" },
@@ -25,18 +27,21 @@ public static class TranslatedModuleHelper
 		{"fi", "%20translated%20full%20(Suomi)" },
 		{"sv", "%20translated%20full%20(Svenska)" },
 		{"th", "%20translated%20full%20(%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B2%E0%B9%84%E0%B8%97%E0%B8%A2)" },
+		{"zh-cn", "%20translated%20full%20(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)" },
 	};
 
 	private static readonly Dictionary<string, string> DisplayNameAddons = new Dictionary<string, string>()
 	{
-		{"en", " (English)"},
+		{"en", " (English)" },
 		{"cu", " (Custom)" },
+		{"cs", " (čeština)" },
 		{"da", " (Dansk)" },
 		{"de", " (Deutsch)" },
 		{"et", " (eesti)" },
 		{"es", " (Español)" },
 		{"eo", " (Esperanto)" },
 		{"fr", " (Français)" },
+		{"he", " (עברית)" },
 		{"ko", " (한국어)" },
 		{"it", " (Italiano)" },
 		{"nl", " (Nederlands)" },
@@ -46,7 +51,8 @@ public static class TranslatedModuleHelper
 		{"ru", " (Русский)" },
 		{"fi", " (Suomi)" },
 		{"sv", " (Svenska)" },
-		{"th", " (ไทย)" },
+		{"th", " (ภาษาไทย)" },
+		{"zh-cn", " (简体中文)" },
 	};
 
 	public static string GetModuleDisplayNameAddon(BombComponent bombComponent)
