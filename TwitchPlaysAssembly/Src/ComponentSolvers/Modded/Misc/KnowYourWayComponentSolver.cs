84422d00ca48ccf6f68806bd1405cd5017bbfa03
samfun123 <samdinner@hotmail.com>
2018-09-11 22:57:05 -0400

    Fix Know Your Way support

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/KnowYourWayComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/KnowYourWayComponentSolver.cs
@@ -11,10 +11,8 @@ public class KnowYourWayComponentSolver : ComponentSolver
 		: base (bombCommander, bombComponent)
 	{
 		_component = bombComponent.GetComponent(_componentType);
-		_upButton = (KMSelectable)_upButtonField.GetValue(_component);
-		_rightButton = (KMSelectable)_rightButtonField.GetValue(_component);
-		_downButton = (KMSelectable)_downButtonField.GetValue(_component);
-		_leftButton = (KMSelectable)_leftButtonField.GetValue(_component);
+		_buttons = _buttonFields.Select(field => (KMSelectable) field.GetValue(_component)).ToArray();
+		_textMeshes = _textFields.Select(field => (TextMesh) field.GetValue(_component)).ToArray();
 		modInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), "Press the buttons labeled UDLR with !{0} press UDLR.");
 	}
 
@@ -30,50 +28,31 @@ public class KnowYourWayComponentSolver : ComponentSolver
 		foreach (char character in iterator)
 		{
 			yield return null;
-			yield return DoInteractionClick(DetermineButton(character));
+			for (int i = 0; i < _textFields.Length; i++)
+			{
+				if (_textMeshes[i].text.ToLower()[0] == character)
+				{
+					DoInteractionClick(_buttons[i]);
+					break;
+				}
+			}
 			yield return "trycancel";
 		}
-	}
-
-	private KMSelectable DetermineButton(char character)
-	{
-		if (((TextMesh)_upTextField.GetValue(_component)).text == character.ToString())
-			return _upButton;
-		else if (((TextMesh)_rightTextField.GetValue(_component)).text == character.ToString())
-			return _rightButton;
-		else if (((TextMesh)_downTextField.GetValue(_component)).text == character.ToString())
-			return _downButton;
-		else if (((TextMesh)_leftTextField.GetValue(_component)).text == character.ToString())
-			return _leftButton;
-		else return null;
 	}
 
 	static KnowYourWayComponentSolver()
 	{
 		_componentType = ReflectionHelper.FindType("KnowYourWay");
-		_upButtonField = _componentType.GetField("UpButton", BindingFlags.Public | BindingFlags.Instance);
-		_upTextField = _componentType.GetField("UpText", BindingFlags.Public | BindingFlags.Instance);
-		_rightButtonField = _componentType.GetField("RightButton", BindingFlags.Public | BindingFlags.Instance);
-		_rightTextField = _componentType.GetField("RightText", BindingFlags.Public | BindingFlags.Instance);
-		_downButtonField = _componentType.GetField("DownButton", BindingFlags.Public | BindingFlags.Instance);
-		_downTextField = _componentType.GetField("DownText", BindingFlags.Public | BindingFlags.Instance);
-		_leftButtonField = _componentType.GetField("LeftButton", BindingFlags.Public | BindingFlags.Instance);
-		_leftTextField = _componentType.GetField("LeftText", BindingFlags.Public | BindingFlags.Instance);
+		_buttonFields = _directions.Select(direction => _componentType.GetField(direction + "Button", BindingFlags.Public | BindingFlags.Instance)).ToArray();
+		_textFields = _directions.Select(direction => _componentType.GetField(direction + "Text", BindingFlags.Public | BindingFlags.Instance)).ToArray();
 	}
 
 	private static Type _componentType = null;
-	private static FieldInfo _upButtonField = null;
-	private static FieldInfo _upTextField = null;
-	private static FieldInfo _rightButtonField = null;
-	private static FieldInfo _rightTextField = null;
-	private static FieldInfo _downButtonField = null;
-	private static FieldInfo _downTextField = null;
-	private static FieldInfo _leftButtonField = null;
-	private static FieldInfo _leftTextField = null;
+	private static FieldInfo[] _buttonFields = null;
+	private static FieldInfo[] _textFields = null;
+	private KMSelectable[] _buttons = null;
+	private TextMesh[] _textMeshes = null;
 
-	private readonly KMSelectable _upButton = null;
-	private readonly KMSelectable _rightButton = null;
-	private readonly KMSelectable _downButton = null;
-	private readonly KMSelectable _leftButton = null;
+	private static readonly string[] _directions = new string[4] { "Up", "Right", "Down", "Left" };
 	private readonly object _component = null;
 }
