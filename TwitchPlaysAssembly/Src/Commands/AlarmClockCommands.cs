16fdfe3af834bcad78c5cfc51a51eb6cb8227c76
caitsith2 <d_good@caitsith2.com>
2018-11-14 09:43:48 -0800

    Fix Alarm clock snooze command

--- TwitchPlaysAssembly/Src/Commands/AlarmClockCommands.cs
+++ TwitchPlaysAssembly/Src/Commands/AlarmClockCommands.cs
@@ -5,11 +5,15 @@ using Assets.Scripts.Props;
 /// <summary>Commands for the alarm clock.</summary>
 public static class AlarmClockCommands
 {
+	[Command("snooze")]
+	public static IEnumerator Snooze(TwitchHoldable holdable, string user, bool isWhisper) =>
+		holdable.RespondToCommand(user, "", isWhisper, Snooze(holdable.Holdable.GetComponent<AlarmClock>(), user, isWhisper));
 
-	[Command(@"snooze")]
 	public static IEnumerator Snooze(AlarmClock clock, string user, bool isWhisper)
 	{
 		var onField = typeof(AlarmClock).GetField("isOn", BindingFlags.NonPublic | BindingFlags.Instance);
+		if(onField == null) yield break;
+
 		if (TwitchPlaySettings.data.AllowSnoozeOnly && !TwitchPlaySettings.data.AnarchyMode && !(bool) onField.GetValue(clock))
 			yield break;
 
