a7418e41cfc4a55cb7d01cbfae496e5ee801f5d3
samfun123 <samdinner@hotmail.com>
2018-12-26 23:31:23 -0500

    Split Sea Shells into two commands "label" and "position"

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Asimir/SeaShellsComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Asimir/SeaShellsComponentSolver.cs
@@ -12,67 +12,73 @@ public class SeaShellsComponentSolver : ComponentSolver
 	{
 		_component = module.BombComponent.GetComponent(ComponentType);
 		_buttons = (KMSelectable[]) ButtonsField.GetValue(_component);
-		ModInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), "Press buttons by typing !{0} press alar llama 3. You can submit partial text as long it only matches one button. You can also use the button's position in english reading order. NOTE: Each button press is separated by a space so typing \"burglar alarm\" will press a button twice.");
+		ModInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), "Press buttons by their labels by typing !{0} label alar llama. You can submit partial text as long it only matches one button. Press buttons by their position using !{0} position 3 5 2 1 4. NOTE: Each button press is separated by a space so typing \"burglar alarm\" will press a button twice.");
 	}
 
 	protected internal override IEnumerator RespondToCommandInternal(string inputCommand)
 	{
 		string[] commands = inputCommand.ToLowerInvariant().Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-		if (commands.Length < 2 || !commands[0].Equals("press")) yield break;
+		if (commands.Length < 2) yield break;
 		IEnumerable<string> buttonLabels = _buttons.Select(button => button.GetComponentInChildren<TextMesh>().text.ToLowerInvariant()).ToList();
 
 		if (buttonLabels.Any(label => label == " ")) yield break;
-		{
-			yield return null;
 
-			IEnumerable<string> submittedText = commands.Skip(1);
-			List<KMSelectable> selectables = new List<KMSelectable>();
+		IEnumerable<string> submittedText = commands.Skip(1);
+		List<KMSelectable> selectables = new List<KMSelectable>();
+
+		if (commands[0].EqualsAny("press", "label", "lab", "l"))
+		{
 			foreach (string text in submittedText)
 			{
 				IEnumerable<string> matchingLabels = buttonLabels.Where(label => label.Contains(text)).ToList();
+				string fixedText = text;
+				if (!buttonLabels.Contains(text))
+				{
+					switch (matchingLabels.Count())
+					{
+						case 1:
+							fixedText = matchingLabels.First();
+							break;
+						case 0:
+							yield return $"sendtochaterror There isn't any label that contains \"{text}\".";
+							yield break;
+						default:
+							yield return
+								$"sendtochaterror There are multiple labels that contain \"{text}\": {string.Join(", ", matchingLabels.ToArray())}.";
+							yield break;
+					}
+				}
 
-				int matchedCount = matchingLabels.Count();
+				selectables.Add(_buttons[buttonLabels.IndexOf(label => label == fixedText)]);
+			}
+		}
+		else if (commands[0].EqualsAny("position", "pos", "index", "ind", "i"))
+		{
+			foreach (string text in submittedText)
+			{
 				if (int.TryParse(text, out int index))
 				{
 					if (index < 1 || index > 5) yield break;
 
 					selectables.Add(_buttons[index - 1]);
 				}
-				else
-				{
-					string fixedText = text;
-					if (!buttonLabels.Contains(text))
-					{
-						switch (matchedCount)
-						{
-							case 1:
-								fixedText = matchingLabels.First();
-								break;
-							case 0:
-								yield return $"sendtochaterror There isn't any label that contains \"{text}\".";
-								yield break;
-							default:
-								yield return
-									$"sendtochaterror There are multiple labels that contain \"{text}\": {string.Join(", ", matchingLabels.ToArray())}.";
-								yield break;
-						}
-					}
-
-					selectables.Add(_buttons[buttonLabels.IndexOf(label => label == fixedText)]);
-				}
 			}
+		}
+		else
+		{
+			yield break;
+		}
 
-			int startingStage = (int) StageField.GetValue(_component);
-			foreach (KMSelectable selectable in selectables)
-			{
-				DoInteractionClick(selectable);
+		int startingStage = (int) StageField.GetValue(_component);
+		foreach (KMSelectable selectable in selectables)
+		{
+			DoInteractionClick(selectable);
 
-				yield return new WaitForSeconds(0.1f);
+			yield return new WaitForSeconds(0.1f);
 
-				if (startingStage != (int) StageField.GetValue(_component))
-					yield break;
-			}
+			if (startingStage != (int) StageField.GetValue(_component))
+				yield break;
 		}
 	}
 
