f1c6be22964fe359f97e7ca686e168954ef0cd22
red031000 <rubenru09@aol.com>
2019-01-04 23:39:06 +0000

    fix translated manual codes

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/TranslatedModules/TranslatedModuleHelper.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/TranslatedModules/TranslatedModuleHelper.cs
@@ -8,26 +8,26 @@ public static class TranslatedModuleHelper
 	{
 		{"en", "" },
 		{"cu", null },
-		{"cs", "%20translated%20full%20(%C4%8De%C5%A1tina)" },
-		{"da", "%20translated%20full%20(Dansk)" },
-		{"de", "%20translated%20full%20(Deutsch)" },
-		{"et", "%20translated%20full%20(eesti)" },
-		{"es", "%20translated%20full%20(Espa%C3%B1ol)" },
-		{"eo", "%20translated%20full%20(Esperanto)" },
-		{"fr", "%20translated%20full%20(Fran%C3%A7ais)" },
-		{"he", "%20translated%20full%20(%D7%A2%D7%91%D7%A8%D7%99%D7%AA)" },
-		{"ko", "%20translated%20full%20(%ED%95%9C%EA%B5%AD%EC%96%B4)" },
-		{"it", "%20translated%20full%20(Italiano)" },
-		{"nl", "%20translated%20full%20(Nederlands)" },
-		{"no", "%20translated%20full%20(Norsk)" },
-		{"jp", "%20translated%20full%20(%E6%97%A5%E6%9C%AC%E8%AA%9E)" },
-		{"pl", "%20translated%20full%20(Polski)" },
-		{"pt-br", "%20translated%20full%20(Portugu%C3%AAs%20do%20Brasil)" },
-		{"ru", "%20translated%20full%20(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)" },
-		{"fi", "%20translated%20full%20(Suomi)" },
-		{"sv", "%20translated%20full%20(Svenska)" },
-		{"th", "%20translated%20full%20(%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B2%E0%B9%84%E0%B8%97%E0%B8%A2)" },
-		{"zh-cn", "%20translated%20full%20(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)" },
+		{"cs", "%20translated%20(%C4%8De%C5%A1tina%20%E2%80%94%20*)" },
+		{"da", "%20translated%20(Dansk%20%E2%80%94%20*)" },
+		{"de", "%20translated%20(Deutsch%20%E2%80%94%20*)" },
+		{"et", "%20translated%20(eesti%20%E2%80%94%20*)" },
+		{"es", "%20translated%20(Espa%C3%B1ol%20%E2%80%94%20*)" },
+		{"eo", "%20translated%20(Esperanto%20%E2%80%94%20*)" },
+		{"fr", "%20translated%20(Fran%C3%A7ais%20%E2%80%94%20*)" },
+		{"he", "%20translated%20(%D7%A2%D7%91%D7%A8%D7%99%D7%AA%20%E2%80%94%20*)" },
+		{"ko", "%20translated%20(%ED%95%9C%EA%B5%AD%EC%96%B4%20%E2%80%94%20*)" },
+		{"it", "%20translated%20(Italiano%20%E2%80%94%20*)" },
+		{"nl", "%20translated%20(Nederlands%20%E2%80%94%20*)" },
+		{"no", "%20translated%20(Norsk%20%E2%80%94%20*)" },
+		{"jp", "%20translated%20(%E6%97%A5%E6%9C%AC%E8%AA%9E%20%E2%80%94%20*)" },
+		{"pl", "%20translated%20(Polski%20%E2%80%94%20*)" },
+		{"pt-br", "%20translated%20(Portugu%C3%AAs%20do%20Brasil%20%E2%80%94%20*)" },
+		{"ru", "%20translated%20(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9%20%E2%80%94%20*)" },
+		{"fi", "%20translated%20(Suomi%20%E2%80%94%20*)" },
+		{"sv", "%20translated%20(Svenska%20%E2%80%94%20*)" },
+		{"th", "%20translated%20(%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B2%E0%B9%84%E0%B8%97%E0%B8%A2%20%E2%80%94%20*)" },
+		{"zh-cn", "%20translated%20(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87%20%E2%80%94%20*)" },
 	};
 
 	private static readonly Dictionary<string, string> DisplayNameAddons = new Dictionary<string, string>()
@@ -60,10 +60,7 @@ public static class TranslatedModuleHelper
 	{
 		try
 		{
-			FieldInfo langField = componentType.GetField("lang", BindingFlags.NonPublic | BindingFlags.Instance);
-			object langObject = langField?.GetValue(component);
-			FieldInfo languageField = langObject?.GetType().GetField("languageId", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-			string languageCode = (string) languageField?.GetValue(langObject);
+			string languageCode = GetLanguageCode(component, componentType);
 			return languageCode == null || !DisplayNameAddons.TryGetValue(languageCode, out string code) ? "" : code;
 		}
 		catch (Exception ex)
@@ -77,11 +74,24 @@ public static class TranslatedModuleHelper
 	{
 		try
 		{
+			string languageCode = GetLanguageCode(component, componentType);
+			return languageCode == null || !ManualCodeAddons.TryGetValue(languageCode, out string code) ? "" : code;
+		}
+		catch (Exception ex)
+		{
+			DebugHelper.LogException(ex, "Could not get the Language of this translated module due to an exception:");
+			return "";
+		}
+	}
+
+	public static string GetLanguageCode(UnityEngine.Component component, Type componentType)
+	{
+		try
+		{
 			FieldInfo langField = componentType.GetField("lang", BindingFlags.NonPublic | BindingFlags.Instance);
 			object langObject = langField?.GetValue(component);
 			FieldInfo languageField = langObject?.GetType().GetField("languageId", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-			string languageCode = (string) languageField?.GetValue(langObject);
-			return languageCode == null || !ManualCodeAddons.TryGetValue(languageCode, out string code) ? "" : code;
+			return (string)languageField?.GetValue(langObject);
 		}
 		catch (Exception ex)
 		{
