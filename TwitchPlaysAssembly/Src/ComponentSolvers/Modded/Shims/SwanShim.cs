71f8e40833224bf083585ae96388880a7dfc2ef1
Kaito Sinclaire <mattwcstrfan@gmail.com>
2019-11-22 23:42:44 -0800

    Scoring changes 11/23 Swan shimmed to give PPA 1 on successful system reset

---
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Shims/SwanShim.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+
+public class SwanShim : ComponentSolverShim
+{
+	public SwanShim(TwitchModule module)
+		: base(module, "theSwan")
+	{
+		_component = Module.BombComponent.GetComponent(ComponentType);
+		ModInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType());
+	}
+
+	protected override IEnumerator RespondToCommandShimmed(string inputCommand)
+	{
+		int resetsPreCommand = _component.GetValue<int>("systemResetCounter");
+
+		IEnumerator command = RespondToCommandUnshimmed(inputCommand);
+		while (command.MoveNext())
+			yield return command.Current;
+
+		// Award a point upon a successful system reset.
+		if (_component.GetValue<int>("systemResetCounter") != resetsPreCommand)
+			yield return "awardpoints 1";
+	}
+
+	static SwanShim()
+	{
+		ComponentType = ReflectionHelper.FindType("theSwanScript");
+	}
+
+	private static readonly Type ComponentType;
+	private readonly object _component;
+}
