a3f8b4cbba5aad37f5d24de8a5693871c106557d
samfun123 <samdinner@hotmail.com>
2019-12-18 17:28:35 -0500

    Solve Encrypted Bingo first during a !solvebomb

--- TwitchPlaysAssembly/Src/Commands/GameCommands.cs
+++ TwitchPlaysAssembly/Src/Commands/GameCommands.cs
@@ -505,7 +505,7 @@ static class GameCommands
 
 		var modules = TwitchGame.Instance.Modules
 			.Where(x => GameRoom.Instance.IsCurrentBomb(x.BombID))
-			.OrderByDescending(module => module.Solver.ModInfo.moduleID.EqualsAny("cookieJars", "organizationModule"));
+			.OrderByDescending(module => module.Solver.ModInfo.moduleID.EqualsAny("cookieJars", "organizationModule", "encryptionBingo"));
 		foreach (var module in modules)
 			if (!module.Solved)
 				module.SolveSilently();
