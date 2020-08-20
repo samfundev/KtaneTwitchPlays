8e73e3d5bd7b2f80bfa66214eb852b71e0276eeb
samfundev <samdinner@hotmail.com>
2020-08-20 19:14:35 -0400

    Update Speak English support for the reuploaded version
    Closes #539

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/SpeakEnglishComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/SpeakEnglishComponentSolver.cs
@@ -1,42 +1,21 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text.RegularExpressions;
 
-public class SpeakEnglishComponentSolver : ComponentSolver
+public class SpeakEnglishComponentSolver : ReflectionComponentSolver
 {
 	public SpeakEnglishComponentSolver(TwitchModule module)
-		: base(module)
+		: base(module, "speakEnglishScript", "Press the top button with !{0} press top.")
 	{
-		object component = module.BombComponent.GetComponent(ComponentType);
-		_buttons = (List<KMSelectable>) ButtonsField.GetValue(component);
-		ModInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType(), "Press the top button with !{0} press top.");
 	}
 
-	protected internal override IEnumerator RespondToCommandInternal(string inputCommand)
+	private readonly string[] buttonNames = new string[] { "top", "middle", "bottom" };
+
+	public override IEnumerator Respond(string[] split, string command)
 	{
-		inputCommand = inputCommand.Trim().ToLowerInvariant();
-		Match match1 = Regex.Match(inputCommand, @"^\s*press\s+(?<index>top|middle|bottom)\s*$");
-		if (!match1.Success) yield break;
+		if (split.Length != 2 || split[0] != "press" || !split[1].EqualsAny(buttonNames))
+			yield break;
+
 		yield return null;
-		// ReSharper disable once SwitchStatementMissingSomeCases
-		switch (match1.Groups["index"].Value)
-		{
-			case "top":
-				yield return DoInteractionClick(_buttons[0]);
-				break;
-			case "middle":
-				yield return DoInteractionClick(_buttons[1]);
-				break;
-			case "bottom":
-				yield return DoInteractionClick(_buttons[2]);
-				break;
-		}
+		yield return Click(Array.IndexOf(buttonNames, split[1]));
 	}
-
-	private static readonly Type ComponentType = ReflectionHelper.FindType("SpeakEnglishBehav");
-	private static readonly FieldInfo ButtonsField = ComponentType.GetField("buttons", BindingFlags.Public | BindingFlags.Instance);
-
-	private readonly List<KMSelectable> _buttons;
 }
