69f5fcb219721ac0a126b66f2f1fbf3f2ec907fc
samfundev <samdinner@hotmail.com>
2020-10-30 23:44:18 -0400

    Move runas to the right spot

--- TwitchPlaysAssembly/Src/Commands/GlobalCommands.cs
+++ TwitchPlaysAssembly/Src/Commands/GlobalCommands.cs
@@ -14,14 +14,6 @@ using Random = UnityEngine.Random;
 /// <summary>Commands that can generally be used at any time.</summary>
 static class GlobalCommands
 {
-	
-	/// <name>Run as</name>
-	/// <syntax>runas [color] [username]#[discrminator] [command]</syntax>
-	/// <summary>Runs a command as the specified Discord user</summary>
-	[Command(@"runas (#.{6}) (.*?)#([0-9]{4}) (!.*?)$", AccessLevel.Streamer, AccessLevel.Streamer)]
-	public static void RunCommandAs([Group(1)] string color, [Group(2)] string username, [Group(3)] string discriminator, [Group(4)] string command) =>
-		IRCConnection.ReceiveMessage($"{username}#{discriminator}", color, command);
-	
 	/// <name>Help</name>
 	/// <syntax>help</syntax>
 	/// <summary>Gives you some help on how to play TP.</summary>
@@ -1250,6 +1242,13 @@ static class GlobalCommands
 	{
 		TwitchPlaysService.Instance.CoroutineQueue.SkipCurrentCoroutine = true;
 	}
+
+	/// <name>Run as</name>
+	/// <syntax>runas [color] [username]#[discrminator] [command]</syntax>
+	/// <summary>Runs a command as the specified Discord user</summary>
+	[Command(@"runas (#.{6}) (.*?)#([0-9]{4}) (!.*?)$", AccessLevel.Streamer, AccessLevel.Streamer)]
+	public static void RunCommandAs([Group(1)] string color, [Group(2)] string username, [Group(3)] string discriminator, [Group(4)] string command) =>
+		IRCConnection.ReceiveMessage($"{username}#{discriminator}", color, command);
 
 	//As of now, Debugging commands are streamer only, apart from whispertest, which are superuser and above.
 	[Command("whispertest", AccessLevel.SuperUser, AccessLevel.SuperUser), DebuggingOnly]
