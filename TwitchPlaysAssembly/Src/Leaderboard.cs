ed8cb8647cc0ab7c1a241067c125b09dc57c9b1f
Sam <samdinner@hotmail.com>
2021-11-11 12:14:41 -0500

    Players who helped with needies should be on the leaderboard
    Fix #569

--- TwitchPlaysAssembly/Src/Leaderboard.cs
+++ TwitchPlaysAssembly/Src/Leaderboard.cs
@@ -208,6 +208,10 @@ public class Leaderboard
 		entry.AddScore(numScore);
 		entry.LastAction = DateTime.Now;
 		ResetSortFlag();
+
+		string name = userName.ToLowerInvariant();
+		if (!CurrentSolvers.ContainsKey(name))
+			CurrentSolvers[name] = 0;
 	}
 
 	public void MakeEvil(string userName) => MakeEvil(userName, SafeGetColor(userName));
