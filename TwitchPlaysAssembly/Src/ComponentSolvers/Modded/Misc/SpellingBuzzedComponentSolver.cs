62b91f0e1e5e20719a25c8f69bb91c77b6c2b8e9
Qkrisi <qkrisiyt@gmail.com>
2022-10-06 17:45:43 +0200

    Add reset command to Spelling Buzzed

--- TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/SpellingBuzzedComponentSolver.cs
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/SpellingBuzzedComponentSolver.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -7,27 +7,33 @@ using UnityEngine;
 public class SpellingBuzzedComponentSolver : ReflectionComponentSolver
 {
 	public SpellingBuzzedComponentSolver(TwitchModule module) :
-		base(module, "SpellingBuzzedModule", "Use !{0} submit DRUNK to submit that word into the module.")
+		base(module, "SpellingBuzzedModule", "Use '!{0} submit DRUNK' to submit that word into the module. Use '!{0} reset' to reset the module.")
 	{
 		_keypadButtons = selectables.ToList();
 		_keypadButtons.RemoveAt(2);
 		_keypadButtons.RemoveAt(7);
-		for (int buttonIndex = 0; buttonIndex < 7; buttonIndex++)
+		for (var buttonIndex = 0; buttonIndex < 7; buttonIndex++)
 			_displayedLetters += _keypadButtons[buttonIndex].transform.Find("ButtonText").GetComponent<TextMesh>().text;
 	}
 
 	public override IEnumerator Respond(string[] split, string command)
 	{
-		command = command.Trim().ToUpperInvariant();
-		string display = _component.GetValue<GameObject>("displayText").GetComponent<TextMesh>().text;
-		Match m = Regex.Match(command, @"^SUBMIT\s+([" + _displayedLetters + "]+)$"); //Will only allow letters on the module.
+		command = command.ToUpperInvariant();
+		if(command.EqualsAny("RESET"))
+		{
+			yield return null;
+			yield return Click(8, 0);
+			yield break;
+		}
+		var m = Regex.Match(command, @"^SUBMIT\s+([" + _displayedLetters + "]+)$"); //Will only allow letters on the module.
 		if (m.Success)
 		{
 			yield return null;
+			var display = _component.GetValue<GameObject>("displayText").GetComponent<TextMesh>().text;
 			if (display.Length != 0 && !m.Groups[1].Value.StartsWith(display))
 				yield return Click(8, 0.15f);
 			display = _component.GetValue<GameObject>("displayText").GetComponent<TextMesh>().text;
-			foreach (char letter in m.Groups[1].Value.Skip(display.Length))
+			foreach (var letter in m.Groups[1].Value.Skip(display.Length))
 			{
 				_keypadButtons[_displayedLetters.IndexOf(letter)].OnInteract();
 				yield return new WaitForSeconds(0.15f);
