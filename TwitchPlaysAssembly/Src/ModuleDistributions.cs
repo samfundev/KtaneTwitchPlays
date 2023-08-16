4c0b7bbdd2525f47ab5b611ee524aee4a420eba7
Kaito Sinclaire <ks@rosenthalcastle.org>
2023-08-16 16:39:38 -0700

    Clean up ModuleDistributions, add Profile pools

--- TwitchPlaysAssembly/Src/ModuleDistributions.cs
+++ TwitchPlaysAssembly/Src/ModuleDistributions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
@@ -15,15 +16,60 @@ public sealed class DistributionPool : ISerializable
 
 	public enum PoolType
 	{
+		// Default pool type or error, gives a warning if attempted to run
 		Invalid,
+
+		// Adds a pool of all enabled solvable modules
+		// Examples:
+		//     "AllSolvable" (fair mix)
+		//     "AllSolvable: Mods" (mods only)
 		AllSolvable,
+
+		// Adds a pool of all enabled needy modules
+		// Examples:
+		//     "AllNeedy" (fair mix)
+		//     "AllNeedy: Mods" (mods only)
 		AllNeedy,
+
+		// Adds a pool of all enabled modules, both solvable and needy
+		// Examples:
+		//     "AllModules" (fair mix including needies)
+		//     "AllModules: Mods" (as above, but mods only)
 		AllModules,
+
+		// Adds a pool containing all solvable modules that fit specific score constraints
+		// Ignores boss modules, or modules that don't have a base score
+		// TODO: Needs some way of accounting for dynamic scoring?
+		// Examples:
+		//     "Score: = 10" (modules with a base score of exactly 10)
+		//     "Score: < 7" (base score of 6 or less)
+		//     "Score: >= 7, <= 13" (base score between 7 and 13)
 		Score,
+
+		// Adds a pool containing all solvable modules enabled by an expert profile
+		// Examples:
+		//     EnabledBy: MyExpertProfile
+		//     EnabledBy: FooProfile, BarProfile (modules enabled by both)
+		ProfileEnables,
+
+		// Adds a pool containing all solvable modules disabled by a defuser profile
+		// Examples:
+		//     DisabledBy: NoBossModules
+		//     DisabledBy: NoColCipher, NoDream (modules disabled by both)
+		ProfileDisables,
+
+		// Adds a fixed pool of module IDs, works like a typical mission pool, meaning:
+		//  - The pool may contain duplicates to make one module more likely to show up
+		//  - All modules in a fixed pool must be enabled
+		// Examples:
+		//     Fixed: spwizTetris (just one module)
+		//     Fixed: brainf, HexiEvilFMN (pick between two)
+		//     Fixed: Wires, Wires, Wires, Wires, Venn (80% Wires, 20% Complicated Wires)
 		Fixed,
 	}
 	private PoolType Type;
-	private List<int> ExtraData;
+	private List<string> Arguments;
+	private Dictionary<string, int> PoolSettings;
 
 	private string __poolDef;
 	public string PoolDefinition
@@ -32,81 +78,71 @@ public sealed class DistributionPool : ISerializable
 		private set
 		{
 			__poolDef = value;
-
 			Type = PoolType.Invalid;
-			ExtraData = new List<int>();
+			Arguments = __poolDef.Split(new char[] {',', ':'}).Select(str => str.Trim()).ToList();
+			PoolSettings = new Dictionary<string, int>();
 
-			List<string> arguments = __poolDef.Split(',').Select(str => str.Trim().ToUpperInvariant()).ToList();
-			string mode = arguments[0];
-			arguments.RemoveAt(0);
+			string mode = Arguments[0].ToUpperInvariant();
+			Arguments.RemoveAt(0);
 
-			bool IsNeedy = false;
+			// Don't change Type until the end, so modes can immediately return to delcare a pool invalid
+			PoolType? FinalType = null;
+
 			switch (mode)
 			{
-				// Examples:
-				//     ALL_SOLVABLE (fair mix)
-				//     ALL_SOLVABLE, MODS (mods only)
 				case "SOLVABLE":
 				case "ALLSOLVABLE":
 				case "ALL_SOLVABLE":
+					FinalType = FinalType ?? PoolType.AllSolvable;
+
 					int ComponentSource = (int) KMComponentPool.ComponentSource.Mods | (int) KMComponentPool.ComponentSource.Base;
-					if (arguments.Count == 1)
+					if (Arguments.Count == 1)
 					{
-						if (arguments[0].Equals("MODS"))
+						if (Arguments[0].ToUpperInvariant().Equals("MODS"))
 							ComponentSource = (int) KMComponentPool.ComponentSource.Mods;
-						else if (arguments[0].Equals("BASE"))
+						else if (Arguments[0].ToUpperInvariant().Equals("BASE"))
 							ComponentSource = (int) KMComponentPool.ComponentSource.Base;
 						else
 							return; // Not valid
 					}
-					else if (arguments.Count >= 2)
+					else if (Arguments.Count >= 2)
 						return; // Also not valid
 
-					Type = IsNeedy ? PoolType.AllNeedy : PoolType.AllSolvable;
-					ExtraData.Add(ComponentSource);
+					PoolSettings["Component Source"] = ComponentSource;
 					break;
 
-				// Examples:
-				//     ALL_NEEDY (fair mix)
-				//     ALL_NEEDY, MODS (mods only)
 				case "NEEDY":
 				case "ALLNEEDY":
 				case "ALL_NEEDY":
-					IsNeedy = true;
+					FinalType = PoolType.AllNeedy;
 					goto case "ALL_SOLVABLE";
 
-				// Examples:
-				//     ALL_MODULES (fair mix including needies)
-				case "ALL_MODULES":
 				case "ALLMODULES":
-					ComponentSource = (int) KMComponentPool.ComponentSource.Mods | (int) KMComponentPool.ComponentSource.Base;
-					Type = PoolType.AllModules;
-					ExtraData.Add(ComponentSource);
-					break;
+				case "ALL_MODULES":
+					FinalType = PoolType.AllModules;
+					goto case "ALL_SOLVABLE";
 
-				// Examples:
-				//     SCORE, =10
-				//     SCORE, <7
-				//     SCORE, >=7, <=13
 				case "SCORE":
+					FinalType = PoolType.Score;
+
 					Match mt;
 					int ScoreMin = int.MinValue, ScoreMax = int.MaxValue;
-					if (arguments.Count == 0 || arguments.Count > 2)
+					if (Arguments.Count == 0 || Arguments.Count > 2)
 						return; // Not valid
 
-					if (arguments.Count == 1 && (mt = Regex.Match(arguments[0], @"^= *(\d+)$")).Success)
+					if (Arguments.Count == 1 && (mt = Regex.Match(Arguments[0], @"^= *(\d+)$")).Success)
 					{
 						if (!int.TryParse(mt.Groups[1].ToString(), out ScoreMin))
-							return;
+							return; // Invalid number - unlikely, but possible
 						ScoreMax = ScoreMin;
 					}
 					else
-						foreach (string arg in arguments)
+						foreach (string arg in Arguments)
 						{
 							if ((mt = Regex.Match(arg, @"^([<>]=?) *(\d+)$")).Success)
 							{
 								if (!int.TryParse(mt.Groups[2].ToString(), out int temp))
-									return;
+									return; // Invalid number - unlikely, but possible
 								switch (mt.Groups[1].ToString())
 								{
 									case ">": ScoreMin = temp + 1; break;
@@ -117,28 +153,36 @@ public sealed class DistributionPool : ISerializable
 								}
 							}
 							else
-								return;
+								return; // Invalid - bad form
 						}
 
-					if (arguments.Count == 2 && (ScoreMin == int.MinValue || ScoreMax == int.MaxValue))
-						return;
+					if (Arguments.Count == 2 && (ScoreMin == int.MinValue || ScoreMax == int.MaxValue))
+						return; // Don't allow conflicting constraints like "< 10, < 50"
 
-					Type = PoolType.Score;
-					ExtraData.Add(ScoreMin);
-					ExtraData.Add(ScoreMax);
+					PoolSettings["Score Min"] = ScoreMin;
+					PoolSettings["Score Max"] = ScoreMax;
 					break;
 
-				// Examples:
-				//     FIXED, spwizTetris
-				//     FIXED, brainf, HexiEvilFMN
-				case "FIXED":
-					if (arguments.Count == 0)
-						return; // Not valid
+				case "ENABLEDBY":
+				case "ENABLED_BY":
+				case "PROFILE_ENABLED_BY":
+					FinalType = PoolType.ProfileEnables;
+					goto case "FIXED";
 
-					// We don't really get to do much, since we're given the entire pool.
-					Type = PoolType.Fixed;
+				case "DISABLEDBY":
+				case "DISABLED_BY":
+				case "PROFILE_DISABLED_BY":
+					FinalType = PoolType.ProfileDisables;
+					goto case "FIXED";
+
+				case "FIXED":
+					FinalType = FinalType ?? PoolType.Fixed;
+					if (Arguments.Count == 0)
+						return; // Not valid
+					// All cases here use the argument list when generating pools
 					break;
 			}
+			Type = FinalType ?? PoolType.Invalid;
 		}
 	}
 
@@ -196,29 +240,35 @@ public sealed class DistributionPool : ISerializable
 			throw new ArgumentOutOfRangeException("Count cannot be negative");
 
 		KMGameInfo gi = TwitchPlaysService.Instance.GetComponent<KMGameInfo>();
+		List<KMGameInfo.KMModuleInfo> AllModules = gi.GetAvailableModuleInfo().ToList();
+
+		// Place a list of KMModuleInfos into this variable, and they'll be added into a ComponentPool at the end.
+		List<KMGameInfo.KMModuleInfo> ModulePool = null;
 
 		switch (Type)
 		{
 			case PoolType.AllSolvable:
 			case PoolType.AllNeedy:
+				// Use the game's built in ALL_SOLVABLE/ALL_NEEDY types.
 				return new KMComponentPool()
 				{
 					SpecialComponentType = (Type == PoolType.AllSolvable
 						? KMComponentPool.SpecialComponentTypeEnum.ALL_SOLVABLE
 						: KMComponentPool.SpecialComponentTypeEnum.ALL_NEEDY),
-					AllowedSources = (KMComponentPool.ComponentSource) ExtraData[0],
+					AllowedSources = (KMComponentPool.ComponentSource) PoolSettings["Component Source"],
 					Count = count
 				};
+	
 			case PoolType.AllModules:
-				List<KMGameInfo.KMModuleInfo> allModules = gi.GetAvailableModuleInfo().ToList();
-				return new KMComponentPool()
-				{
-					ComponentTypes = allModules.Where(x => !x.IsMod).Select(x => x.ModuleType).ToList(),
-					ModTypes = allModules.Where(x => x.IsMod).Select(x => x.ModuleId).ToList(),
-					Count = count
-				};
+				// This one doesn't exist in the game, so we have to make it ourselves.
+				ModulePool = AllModules.Where(Module => {
+					if (Module.IsMod) return (PoolSettings["Component Source"] & (int)KMComponentPool.ComponentSource.Mods) != 0;
+					else              return (PoolSettings["Component Source"] & (int)KMComponentPool.ComponentSource.Base) != 0;
+				}).ToList();
+				break;
+
 			case PoolType.Score:
-				List<KMGameInfo.KMModuleInfo> scoredModules = gi.GetAvailableModuleInfo().Where(x =>
+				ModulePool = AllModules.Where(x =>
 				{
 					if (x.IsNeedy)
 						return false;
@@ -229,46 +279,54 @@ public sealed class DistributionPool : ISerializable
 						return false;
 
 					var baseScore = baseMethod.Points;
-					return baseScore >= ExtraData[0] && baseScore <= ExtraData[1];
+					return baseScore >= PoolSettings["Score Min"] && baseScore <= PoolSettings["Score Max"];
 				}).ToList();
+				break;
 
-				if (scoredModules.Count == 0)
-					throw new InvalidOperationException("There are no enabled modules that fit the score requirements.");
+			case PoolType.ProfileEnables:
+			case PoolType.ProfileDisables:
+				HashSet<string> relevantMods = new HashSet<string>();
 
-				return new KMComponentPool()
+				foreach (string ProfileName in Arguments)
 				{
-					ComponentTypes = scoredModules.Where(x => !x.IsMod).Select(x => x.ModuleType).ToList(),
-					ModTypes = scoredModules.Where(x => x.IsMod).Select(x => x.ModuleId).ToList(),
-					Count = count
-				};
-			case PoolType.Fixed:
-				List<string> moduleIDs = __poolDef.Split(',').Select(str => str.Trim()).ToList();
-				moduleIDs.RemoveAt(0); // Remove "FIXED"
+					ProfileHelper.Profile data;
+					string profilePath = Path.Combine(ProfileHelper.ProfileFolder, ProfileName + ".json");
+					try { data = ProfileHelper.GetProfile(profilePath); }
+					catch (FileNotFoundException) { throw new InvalidOperationException($"Profile {ProfileName} doesn't exist."); }
 
-				List<string> availableMods = gi.GetAvailableModuleInfo().Where(x => x.IsMod).Select(x => x.ModuleId).ToList();
-				List<KMComponentPool.ComponentTypeEnum> poolVanillas = new List<KMComponentPool.ComponentTypeEnum>();
-				List<string> poolMods = new List<string>();
-
-				foreach (string module in moduleIDs)
-				{
-					if (Enum.GetNames(typeof(KMComponentPool.ComponentTypeEnum)).Contains(module))
-						poolVanillas.Add((KMComponentPool.ComponentTypeEnum) Enum.Parse(typeof(KMComponentPool.ComponentTypeEnum), module));
-					else
-					{
-						if (!availableMods.Contains(module))
-							throw new InvalidOperationException($"This distribution contains a fixed pool, and at least one of the modules in that pool ({module}) is not enabled.");
-						poolMods.Add(module);
-					}
+					relevantMods.UnionWith((Type == PoolType.ProfileEnables) ? data.EnabledList : data.DisabledList);
 				}
-				return new KMComponentPool()
-				{
-					ComponentTypes = poolVanillas,
-					ModTypes = poolMods,
-					Count = count
-				};
+
+				// Profiles cannot contain info about vanilla modules, so a profile pool is always mods only.
+				ModulePool = AllModules.Where(x => x.IsMod && !x.IsNeedy && relevantMods.Contains(x.ModuleId)).ToList();
+				break;
+
+			case PoolType.Fixed:
+				// We treat a fixed pool like a regualar pool in a mission; i.e. we allow duplicates for the purposes of
+				// making some modules more likely than others, and if any module is missing then we bail out.
+				Dictionary<string, KMGameInfo.KMModuleInfo> reverseLookup = AllModules.ToDictionary(x => {
+					return x.IsMod ? x.ModuleId : Enum.GetName(typeof(KMComponentPool.ComponentTypeEnum), x.ModuleType);
+				}, x => x);
+
+				ModulePool = Arguments.Select(Module => {
+					if (!reverseLookup.ContainsKey(Module))
+						throw new InvalidOperationException($"This distribution contains a fixed pool, and at least one of the modules in that pool ({Module}) is not enabled.");
+					return reverseLookup[Module];
+				}).ToList();
+				break;
+
 			default:
-				throw new InvalidOperationException("One of the pools in this distribution is not set up properly.");
+				throw new InvalidOperationException($"The following pool definition is invalid: \"{__poolDef}\"");
 		}
+
+		if ((ModulePool?.Count ?? 0) == 0) // Anti-softlock
+			throw new InvalidOperationException($"No enabled modules fit the requirements of the following pool: \"{__poolDef}\"");
+		return new KMComponentPool()
+		{
+			ComponentTypes = ModulePool.Where(x => !x.IsMod).Select(x => x.ModuleType).ToList(),
+			ModTypes = ModulePool.Where(x => x.IsMod).Select(x => x.ModuleId).ToList(),
+			Count = count
+		};
 	}
 
 	public int RewardPointsGiven(int count)
@@ -276,7 +334,7 @@ public sealed class DistributionPool : ISerializable
 		if (RewardPerModule != -1)
 			return RewardPerModule * count;
 
-		if (Type == PoolType.AllSolvable && ExtraData[0] == (int) KMComponentPool.ComponentSource.Base)
+		if (Type == PoolType.AllSolvable && PoolSettings["Component Source"] == (int) KMComponentPool.ComponentSource.Base)
 			return 2 * count;
 		return 5 * count;
 	}
@@ -286,7 +344,7 @@ public sealed class DistributionPool : ISerializable
 		if (TimePerModule != -1)
 			return TimePerModule * count;
 
-		if (Type == PoolType.AllSolvable && ExtraData[0] == (int) KMComponentPool.ComponentSource.Base)
+		if (Type == PoolType.AllSolvable && PoolSettings["Component Source"] == (int) KMComponentPool.ComponentSource.Base)
 			return 60 * count;
 		return 120 * count;
 	}
