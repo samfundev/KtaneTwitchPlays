949cafa4add649ed32498e0f36e66020f8279d11
Brendan King <brendan@brendanking.com>
2023-09-16 16:13:09 -0400

    Add new component solver, fix build error, and add shim
    - Added a component solver for Green Wires
    - Added a shim and setting for chip that disables the copyright music
    - Fixed not being able to build due to ReflectionComponentSolver changes causing an error in Spelling Buzzed

---
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Misc/GreenWiresComponentSolver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class GreenWiresComponentSolver : ReflectionComponentSolver
+{
+	public GreenWiresComponentSolver(TwitchModule module) :
+		base(module, "GreenWires", "!{0} cut <1-7> [Cuts the specified wire]")
+	{
+	}
+
+	public override IEnumerator Respond(string[] split, string command)
+	{
+		if (split.Length != 2 || !command.StartsWith("cut ")) yield break;
+		if (!int.TryParse(split[1], out int check)) yield break;
+		if (check < 1 || check > 7) yield break;
+
+		yield return null;
+		yield return Click(check - 1, 0);
+	}
+
+	protected override IEnumerator ForcedSolveIEnumerator()
+	{
+		yield return null;
+
+		List<int> validWires = _component.GetValue<List<int>>("cutable_wires");
+		int choice = UnityEngine.Random.Range(0, validWires.Count);
+		yield return Click(validWires[choice]);
+	}
+}
---
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Shims/Misc/ChipShim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class ChipShim : ComponentSolverShim
+{
+	public ChipShim(TwitchModule module)
+		: base(module)
+	{
+		ModInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType());
+		if (TwitchPlaySettings.data.DisableCopyrightChipMusic)
+			module.BombComponent.GetComponent(ComponentType).gameObject.GetComponentInChildren<AudioSource>().mute = true;
+	}
+
+	private static readonly Type ComponentType = ReflectionHelper.FindType("chip", "chip");
+}
