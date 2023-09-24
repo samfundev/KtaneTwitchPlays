8be01f3bd1c238422daa3fa13f76a6f4af5be063
Brendan King <brendan@brendanking.com>
2023-09-24 03:24:09 -0400

    Add 3 shims to fix old broken module commands
    These modules don't seem like they are getting a fix from their authors anytime soon so I'm putting the fixes TP side instead.
    
    - Fixed Alien Filing Colors skipping to getting the valid number positions in the command and not checking for any invalid characters that the help message doesnt use.
    - Fixed Double-On throwing an exception if you tried to specify a non-existent LED due to a wrong array length being used in the original code.
    - Fixed Simon's Satire throwing an exception on the normal press command due to the command parameter length not being checked properly in the original code.

---
+++ TwitchPlaysAssembly/Src/ComponentSolvers/Modded/Shims/Misc/AlienFilingColorsShim.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Linq;
+
+public class AlienFilingColorsShim : ReflectionComponentSolverShim
+{
+	public AlienFilingColorsShim(TwitchModule module)
+		: base(module, "AFCScript", "AlienModule")
+	{
+		ModInfo = ComponentSolverFactory.GetModuleInfo(GetModuleType());
+	}
+
+	protected override IEnumerator RespondShimmed(string[] split, string command)
+	{
+		if (!command.EqualsAny("colorblind", "colourblind", "cb"))
+		{
+			foreach (char c in command)
+			{
+				if (!_validChars.Contains(c))
+					yield break;
+			}
+		}
+		yield return RespondUnshimmed(command);
+	}
+
+	private readonly char[] _validChars = { '1', '2', '3', '4', '5', '6', '7', '8', ' ', ',', '|', '-' };
+}
